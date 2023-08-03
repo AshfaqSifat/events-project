04afdae1900d557f603637640776d614cc8974c1
AshfaqSifat <ashfaqs2007@gmail.com>
2023-08-03 15:56:09 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let aqua = document.querySelector("#aqua");
+let teal = document.querySelector("#teal");
+let pink = document.querySelector("#pink");
+let purple = document.querySelector("#purple");
+let colorless = document.querySelector("#colorless");
+let div = document.querySelector("div");
+
+aqua.onclick = function() {
+    div.style.backgroundColor = "aqua";
+    div.innerHTML = "aqua";
+}
+
+teal.onclick = function() {
+    div.style.backgroundColor = "teal";
+    div.innerHTML = "teal";
+}
+
+pink.onclick = function() {
+    div.style.backgroundColor = "pink";
+    div.innerHTML = "pink";
+}
+
+purple.onclick = function() {
+    div.style.backgroundColor = "purple";
+    div.innerHTML = "purple";
+}
+
+colorless.onclick = function() {
+    div.style.backgroundColor = "white";
+    div.innerHTML = "colorless";
+}
